2a8e5685101e7fc3a4b495b915c6e991a37bdc25
Adam Maser <acmaser12@gmail.com>
2020-01-26 15:42:32 -0600

    Added outline for program, will guide coding from here

--- src/Coordinator.java
+++ src/Coordinator.java
@@ -1,9 +1,23 @@
-/**
- * Adam Maser
- * CSC 450
- * Assignment 2
- *
- * 1.26.2020
+/*
+  Adam Maser
+  CSC 450
+  Assignment 2
+
+  1.26.2020
+ */
+
+/*
+ -------
+  Program Outline
+ -------
+  1) Load data from file
+       a) store each band into Band object
+  2) Sort Bands by name and set time (2 ArrayLists?)
+       a) Tower of Hanoi algorithm?
+  3) Method for binary Band Name search (searchBands())
+       a) recursively search
+  4) Method for linear Set Time search (searchSetTime())
+
  */
 
 import java.util.ArrayList;
